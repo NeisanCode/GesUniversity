044325c020ba6fe4f6cbfe3694deab5b882c881d
Neisan <misterneisan0@gmail.com>
2026-08-06 22:50:55 +0100

    refactor(imports): reorganize student form imports and remove obsolete files

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -1,3 +1,3 @@
 from .menu_app import MenuApp
-from .form.student_list_form import StudentListFormFrame
-from .form.student_edit_level import StudentEditModalLevel
+from .form.student_list.student_list_form import StudentListFormFrame
+from .form.student_list.student_edit_level import StudentEditModalLevel
