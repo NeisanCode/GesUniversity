53e64c8ad922042d5bb85a17d7b7813a0e190111
Neisan <misterneisan0@gmail.com>
2026-08-07 22:45:46 +0100

    refactor(logo_conf): update BASE_DIR calculation for improved path resolution and add LOGO_PATH print statement

--- config/logo_conf.py
+++ config/logo_conf.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-# Récupère le dossier racine du .exe (ou du script .py en dev)
 if getattr(sys, "frozen", False):
     BASE_DIR = os.path.dirname(sys.executable)
 else:
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Chemin portable et dynamique vers le logo
 LOGO_PATH = os.path.join(BASE_DIR, "assets", "app_logo.png")
+
+print(LOGO_PATH)
